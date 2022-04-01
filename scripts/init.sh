f3ec2df34c69ec374c348489ae3df80bafb920e4
Seversonic <rm.severs@gmail.com>
2022-03-31 21:00:23 -0600

    cron cleanup

--- scripts/init.sh
+++ scripts/init.sh
@@ -12,8 +12,7 @@ mega-whoami
 /megacmd/scripts/sync.sh >> /proc/1/fd/1
 
 #set cron jobs
-crontab /megacmd/config/cron/cron-pause
-crontab /megacmd/config/cron/cron-resume
+crontab /megacmd/config/cron
 cron -f
 
 #keep container alive

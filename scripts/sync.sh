f89e00d9ca028bbd217c695b179aae50f17122ca
Seversonic <rm.severs@gmail.com>
2022-04-01 19:47:32 -0600

    fixed sync file, changed to LF EOL

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -2,5 +2,5 @@
 while IFS=',' read -r source remote;
         do 
                 echo "Syncing:" "$source" "to Mega path:" "$remote";
-                mega-sync "$source" "${remote::-1}" >> /proc/1/fd/1 ; #trims EOL character
+                mega-sync "$source" "$remote" >> /proc/1/fd/1 ; #trims EOL character
 done < /megacmd/config/syncs
